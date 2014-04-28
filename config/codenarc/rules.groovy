714482d9c1d0fdbc6abdf47d1f0ff9e5c9fc1ad4
Stefan Wolf <stefan.wolf@tngtech.com>
2014-04-28 20:22:08 +0200

    Add Codenarc rules file

---
+++ config/codenarc/rules.groovy
@@ -0,0 +1,15 @@
+// File: config/codenarc/rules.groovy
+
+ruleset {
+    ruleset('rulesets/basic.xml')
+    ruleset('rulesets/braces.xml')
+    ruleset('rulesets/exceptions.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/logging.xml') {
+        'Println' priority: 1
+        'PrintStackTrace' priority: 1
+    }
+    ruleset('rulesets/naming.xml')
+    ruleset('rulesets/unnecessary.xml')
+    ruleset('rulesets/unused.xml')
+}
